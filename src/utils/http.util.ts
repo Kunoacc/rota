0cb32b3d41395010fc18418c427ec9411cf5a3e0
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2020-12-14 15:48:18 +0100

    feat (util): Added fetch utility method

---
+++ src/utils/http.util.ts
@@ -0,0 +1,24 @@
+interface HttpResponse<T> extends Response {
+  parsedBody?: T
+}
+
+const BASE_PATH: string = `https://clava.io/api/`
+
+export async function http<T>(
+  path: RequestInfo
+): Promise<HttpResponse<T>> {
+  const response: HttpResponse<T> = await fetch( `${BASE_PATH}/${path}`,{
+    credentials: 'omit',
+    method: 'GET',
+    cache: 'no-cache'
+  });
+  try {
+    response.parsedBody = await response.json();
+  } catch (error) {}
+
+  if (!response.ok || response.status !== 200) {
+    throw new Error(response.statusText)
+  }
+
+  return response;
+}
